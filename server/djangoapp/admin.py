48a6c9b4351c0b7775c2314430650efe95945b5d
ps-000 <162001982+ps-000@users.noreply.github.com>
2024-05-03 19:17:43 +0530

    Update admin.py

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from .models import CarMake, CarModel
 
-
 # Register your models here.
 admin.site.register(CarMake)
 admin.site.register(CarModel)
@@ -12,4 +11,4 @@ admin.site.register(CarModel)
 # CarMakeAdmin class with CarModelInline
 
 # Register models here
-from .models import CarMake, CarModel
+
