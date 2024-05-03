166a346068b23829f978cefcc927d5f30d11feda
ps-000 <162001982+ps-000@users.noreply.github.com>
2024-05-03 19:56:44 +0530

    Update views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -2,10 +2,7 @@
 
 from django.shortcuts import render
 from django.contrib.auth.models import User
-from django.shortcuts import get_object_or_404, render, redirect
 from django.contrib.auth import logout
-from django.contrib import messages
-from datetime import datetime
 from .models import CarMake, CarModel
 
 from django.http import JsonResponse
