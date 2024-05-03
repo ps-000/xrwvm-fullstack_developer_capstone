1742beb2c9d1465dcce8e54c0ef2d8ce8a2e59ab
ps-000 <162001982+ps-000@users.noreply.github.com>
2024-05-03 19:40:21 +0530

    Update populate.py

--- server/djangoapp/populate.py
+++ server/djangoapp/populate.py
@@ -17,9 +17,18 @@ def initiate():
 
     # Create CarModel instances with the corresponding CarMake instances
     car_model_data = [
-      {"name": "Pathfinder", "type": "SUV", "year": 2023, "car_make": car_make_instances[0]},
-      {"name": "Qashqai", "type": "SUV", "year": 2023, "car_make": car_make_instances[0]},
-      {"name": "XTRAIL", "type": "SUV", "year": 2023, "car_make": car_make_instances[0]},
+      {"name": "Pathfinder",
+       "type": "SUV",
+       "year": 2023,
+       "car_make": car_make_instances[0]},
+      {"name": "Qashqai",
+       "type": "SUV",
+       "year": 2023,
+       "car_make": car_make_instances[0]},
+      {"name": "XTRAIL",
+       "type": "SUV",
+       "year": 2023,
+       "car_make": car_make_instances[0]},
       {"name": "A-Class", "type": "SUV", "year": 2023, "car_make": car_make_instances[1]},
       {"name": "C-Class", "type": "SUV", "year": 2023, "car_make": car_make_instances[1]},
       {"name": "E-Class", "type": "SUV", "year": 2023, "car_make": car_make_instances[1]},
